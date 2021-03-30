6ec10a3ebe11d6d43d425d685af925ab0e425942
rhizomacomp10 <48740591+rhizomacomp10@users.noreply.github.com>
2021-03-29 23:14:19 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+## installs the dependencies
+
+from os import system
+
+os = platform.system()
+
+if os == "Windows":
+  system('pip install discord-webhook')
+  system('pip install selenium')
+  system('pip install requests')
+  print('\nFinished Installing dependencies.')
+else:
+  system('apt update')
+  system('apt install chromium-chromedriver')
+  system('pip install discord-webhook')
+  system('pip install selenium')
+  system('pip install requests')
+  print('\nFinished Installing dependencies.')
